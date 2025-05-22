fb8b7f02df319d30da73c7ce739142544e2e3eef
msara1111 <sara.metalli@gmail.com>
2025-05-22 18:35:19 +0200

    navbar created

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import logo from '../logo.png';
+import './Navbar.css';
+
+
+function Navbar() {
+    return (
+      <div className="navbar">
+        <div className="navbar">
+            <img src={logo} alt="Logo" className="navbar__logo" />
+        </div>
+      </div>
+    );
+  }
+  
+export default Navbar;
