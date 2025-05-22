7380a7077a7801294fc6d7c2153cce7248430a74
msara1111 <sara.metalli@gmail.com>
2025-05-22 19:36:20 +0200

    Refactor Navbar component: wrap link text in paragraph tags and adjust CSS margins

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,9 @@ function Navbar() {
     <div className="navbar">
       <img src={logo} alt="Logo" className="navbar__logo" />
       <div className='navbar__links'>
-        <div>HOME</div>
-        <div>ABOUT ME</div>
-        <div>CONTACTS</div>
+        <div><p>HOME</p></div>
+        <div><p>ABOUT ME</p></div>
+        <div><p>CONTACTS</p></div>
       </div>
     </div>
   );
