37af3fc85e380c801f6c98edcb6eace29611c462
msara1111 <sara.metalli@gmail.com>
2025-05-23 16:17:11 +0200

    Implement snackbar for "Coming Soon" notifications in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,33 @@
 import './Navbar.css';
 import logo from '../logo.png';
 import { useNavigate } from 'react-router-dom';
+import Snackbar from '../components/Snackbar';
+import { useState } from 'react';
 
 function Navbar() {
+  const [snackbarVisible, setSnackbarVisible] = useState(false);
   const navigate = useNavigate();
   const handleClick = (page) => {
     navigate(page);
   };
-
+  const handleComingSoon = () => {
+      setSnackbarVisible(true);
+      setTimeout(() => setSnackbarVisible(false), 2500);
+    };
 
   return (
+    <>
     <div className="navbar">
       <img src={logo} alt="Logo" className="navbar__logo" onClick={() => handleClick('/')}
  />
       <div className='navbar__links'>
-        <div><p>HOME</p></div>
-        <div><p>ABOUT ME</p></div>
-        <div><p>CONTACTS</p></div>
+        <div onClick={() => {handleClick("/")}}><p>HOME</p></div>
+        <div onClick={handleComingSoon}><p>ABOUT ME</p></div>
+        <div onClick={handleComingSoon}><p>CONTACTS</p></div>
       </div>
     </div>
+    <Snackbar message="Coming soon!" visible={snackbarVisible} />
+    </>
   );
 }
 
